35c09bfc41581914f199d3a12c754e9feabe9146
Nanimnet <nana@markonline.ru>
2022-06-24 16:28:37 +0200

    test 14-8

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 app.use(errors());
 
-// app.use(errorsHandler);
+app.use(errorsHandler);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
